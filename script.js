5f693fa6b2594fdfec6d5f1fcda97ab052391e34
bszeidl <szeidl.balazs@hotmail.com>
2022-12-25 20:09:53 +0100

    First line ready.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+    
+    function star (positionX, positionY, size) {
+
+        context.beginPath();
+        context.moveTo(positionX,positionY);
+        context.lineTo(positionX + size * .5,positionY - size * .4);
+        context.lineTo(positionX,positionY);
+
+        context.fillStyle = 'rgb(233,159,184)';
+        context.fill();
+        context.strokeStyle = 'rgb(233,159,184)';
+        context.stroke();
+
+    }
+
+    
+    star(250, 220, 150);
 
 }
 
