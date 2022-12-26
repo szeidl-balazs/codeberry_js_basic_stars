c4016490819a5721be0f21d5a337b175d19b5fb1
bszeidl <szeidl.balazs@hotmail.com>
2022-12-26 19:20:34 +0100

    The 3 stars are completed.

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ function pageLoaded () {
     function star (positionX, positionY, size) {
 
         context.beginPath();
-        context.moveTo(positionX,positionY);
-        context.lineTo(positionX + size * .5,positionY - size * .4);
-        context.lineTo(positionX,positionY);
+        
+        context.moveTo(positionX,positionY); /*bal felső csúcs kiindulás*/
+        context.lineTo(positionX + size, positionY); /*jobb felső csúcs*/
+        context.lineTo(positionX + size * .15, positionY + size * .5); /*bal alsó csúcs*/
+        
+
+
+        context.moveTo(positionX + size * .15, positionY + size * .5); /*bal alsó csúcs*/
+        context.lineTo(positionX + size * .5, positionY - size * .4); /*felső csúcs*/
+        context.lineTo(positionX + size - size * .15, positionY + size * .5); /*jobb alsó csúcs*/
+        context.lineTo(positionX,positionY); /*bal felső csúcs kiindulás*/
+
+        
+        
 
         context.fillStyle = 'rgb(233,159,184)';
         context.fill();
@@ -16,7 +27,8 @@ function pageLoaded () {
 
     }
 
-    
+    star(40, 50, 75);
+    star(130, 120, 100);
     star(250, 220, 150);
 
 }
